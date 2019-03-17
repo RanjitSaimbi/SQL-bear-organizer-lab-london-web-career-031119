e26b1bcfcdd9720bfbae0ca21e688d10efe40d5b
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-17 17:29:04 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 3, 'M', "black", "aggressive", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 3, 'M', "black", "aggressive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 1, 'M', "white", "aggressive", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Taibitha', 3, 'M', "black", "aggressive", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Taibitha', 8, 'F', "brown", "gentle", 0);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 3, 'M', "black", "aggressive", 0);
 
