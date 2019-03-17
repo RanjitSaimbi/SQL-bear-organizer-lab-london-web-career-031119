b9e26e942f8a3cef72d2f2473fcd9eb93aa78eca
Ranjit Saimbi <ranjit.saimbi@gmail.com>
2019-03-17 17:29:57 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -12,4 +12,4 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 3, 'M', "black", "aggressive", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 3, 'M', "black", "aggressive", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 15, 'M', "black", "aggressive", 1);
